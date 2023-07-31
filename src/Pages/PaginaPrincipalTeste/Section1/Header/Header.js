35e5e62a48b6afda47faf8a6605b992c1859a6f5
Pedro-Facchinetti <pedrofacchinetti2021@gmail.com>
2023-07-31 14:16:41 -0300

    remoção de feature problemática

--- src/Pages/PaginaPrincipalTeste/Section1/Header/Header.js
+++ src/Pages/PaginaPrincipalTeste/Section1/Header/Header.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import styles from './header.module.scss';
-import { LogoUni } from 'Components/PaginaPrincipal/Header/LogoUni/index'
-import { LogoUniSmall } from 'Components/PaginaPrincipal/Header/LogoUniSmall';
+import { LogoUni } from 'Components/PaginaPrincipal/Header/LogoUni/index'  
 import { Sobre } from 'Components/PaginaPrincipal/Header/Sobre/index';      
 import { QuemSomos } from 'Components/PaginaPrincipal/Header/QuemSomos/index';
 
 function Header() {
-  const matches = useMediaQuery('(max-width:600px)'); // Aqui é onde definimos a variável 'matches'
-  
   return (
     <div className={styles.topo_Pagina__Conteudo__Cabecalho}>
       <div className={styles.topo_Pagina__Conteudo__Cabecalho__cell}>
-      {matches ? <LogoUniSmall className={styles.uniceplac_Logo} /> : <LogoUni className={styles.uniceplac_Logo} />}
+        <LogoUni className={styles.uniceplac_Logo} />
       </div>
       <div className={styles.topo_Pagina__Conteudo__Cabecalho__spacer} />
       <Sobre className={styles.cabecalho_Sobre} />
