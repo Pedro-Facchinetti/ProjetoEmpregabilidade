121d74b3b212e31a4714324555e844414f63e649
Joao <juniormenezes384@gmail.com>
2023-09-04 22:20:49 -0300

    Utilização de secitons na página de editar pedfil

--- src/Pages/Aluno/EditarPerfil/index.js
+++ src/Pages/Aluno/EditarPerfil/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './EditarPerfil.module.scss'
 
-import img from 'Assets/Aluno/Perfil/img.png'
 import NavButton from './NavButton';
-import Button2 from 'Components/Button2';
-import Input from './Input'
+
+import renderSection1 from './Section1';
+import renderSection2 from './Section2';
 
 function EditarPerfil(props) {
     return (
@@ -19,41 +19,10 @@ function EditarPerfil(props) {
                 </nav>
             </div>
 
-            <div className={styles.container__rigth}>{/** Futuramente pode ser melhor transfirmar estas div's em sections */}
-                <div id='informacoes-pessoais' className={styles.containers}>
-                    <div className={styles.firt_container_info}>
-                        <div className={styles.info__top}>
-                            <h1>Informações pessoais</h1>
-                        </div>
-                        
-                        <div className={styles.info__bottom}>
-                            <div className={styles.info_pessoais_left}>
-                                <img className={styles.img_left_container} src={img} alt='imagem' />
-                                <Button2 label='Editar imagem'></Button2>
-                            </div>
-
-                            <div className={styles.info_pessoais_rigth}>{/** Componentizar os imputs dentro da página */}
-                                <Input></Input>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div id='pela-web' className={styles.containers}>
-
-                </div>
-
-                <div id='sobre-mim' className={styles.containers}>
-
-                </div>
-
-                <div id='experiencias' className={styles.containers}>
-
-                </div>
-
-                <div id='linguas' className={styles.containers}>
-
-                </div>
+            <div className={styles.container__rigth}>
+                <div id='section1' className={styles.containers}>{renderSection1(props)}</div>
+                <div id='section2' className={styles.containers}>{renderSection2(props)}</div>
+                <div id='section3' className={styles.containers}>{renderSection2(props)}</div>
             </div>
         </div>
     )
